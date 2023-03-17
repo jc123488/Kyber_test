e119c3033b980fa561b2632a5e2a6c2f09458463
jason <jasonchou0808@gmail.com>
2023-03-17 17:16:09 +0800

    modify

--- gen_root.py
+++ gen_root.py
@@ -1,6 +1,13 @@
+# import math
+# print("[")
+# for i in range(0,257,1):
+#     print((1175**i)%3329,end='')
+#     print(", ",end='')
+# print("]")
+
 import math
-print("[")
-for i in range(0,257,1):
-    print((1175**i)%3329,end='')
+# print("[")
+for i in range(0,128,1):
+    print((17**i)%3329,end='')
     print(", ",end='')
-print("]")
+# print("]")

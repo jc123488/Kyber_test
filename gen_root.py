087bd07255a6c55c8f6262a74b5aeda5e06278c3
jason <jasonchou0808@gmail.com>
2023-03-23 20:40:50 +0800

    456

--- gen_root.py
+++ gen_root.py
@@ -4,10 +4,19 @@
 #     print((1175**i)%3329,end='')
 #     print(", ",end='')
 # print("]")
-
 import math
-# print("[")
-for i in range(0,128,1):
-    print((17**i)%3329,end='')
-    print(", ",end='')
-# print("]")
+def bit_reverse(n):
+    reversed_n = 0
+    for i in range(7):
+        reversed_n = (reversed_n << 1) | (n & 1)
+        n >>= 1
+    return reversed_n
+
+if __name__ == '__main__' :
+    # print("[")
+    for i in range(0,128,1):
+        r_i=bit_reverse(i)
+        # print(r_i)
+        print((17**r_i)%3329,end='')
+        print(", ",end='')
+    # print("]")
